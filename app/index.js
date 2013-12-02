4f8e683c197c1365829ae66b375c59a2d730631b
Jay Proulx <jason.proulx@pollensoft.com>
2013-12-02 13:58:46 -0500

    Restructure questions, fixed the previous stack trace issue.

--- app/index.js
+++ app/index.js
@@ -1,19 +1,19 @@
 /**
-* app()
-* + What's your application name?
-* + What's your application version?
-* cb();
-* 
-* projectfiles() 
-* If git is installed:
-* + Yes: Is there a .git folder already?
-* + + No: Do you want me to init git for you?
-* + + + No: cb()
-* + + + Yes: Install hooks.
-* + + Yes: Install hooks.
-* + + 
-* + No: cb()
-*/
+ * app()
+ * + What's your application name?
+ * + What's your application version?
+ * cb();
+ *
+ * projectfiles()
+ * If git is installed:
+ * + Yes: Is there a .git folder already?
+ * + + No: Do you want me to init git for you?
+ * + + + No: cb()
+ * + + + Yes: Install hooks.
+ * + + Yes: Install hooks.
+ * + +
+ * + No: cb()
+ */
 
 'use strict';
 
@@ -24,14 +24,14 @@ var cp = require('child_process');
 var fs = require('fs');
 
 function exists(path) {
+	var stats;
+
 	try {
 		// Query the entry
 		stats = fs.lstatSync(path);
 
 		// Is it a directory?
-		if (stats.isDirectory() || stats.isFile()) {
-			return true;
-		}
+		return stats.isDirectory() || stats.isFile();
 	} catch (e) {
 		return false;
 	}
@@ -55,23 +55,20 @@ var UntetheredGenerator = module.exports = function UntetheredGenerator(args, op
 util.inherits(UntetheredGenerator, yeoman.generators.Base);
 
 UntetheredGenerator.prototype.askFor = function askFor() {
-	var async = this.async();
-	var cb = function() { console.log("UNTETHERED: askFor async."); async(); }.bind(this);
+	var async = this.async(),
+		prompts;
 
-	this.gitExists = exists(__dirname, "/.git");
+	this.gitExists = exists(process.cwd() + "/.git");
 
-	// have Yeoman greet the user.
-	console.log(this.yeoman);
-
-	var prompts = [
+	prompts = [
 		{
 			name: 'appName',
-			default: 'My App',
+			'default': 'My App',
 			message: 'What do you want to name your application (hint: keep it short)?'
 		},
 		{
 			name: 'appVersion',
-			default: '0.0.1',
+			'default': '0.0.1',
 			message: 'What version do you want to start with?'
 		}
 	];
@@ -80,7 +77,7 @@ UntetheredGenerator.prototype.askFor = function askFor() {
 		this.appName = props.appName;
 		this.appVersion = props.appVersion;
 
-		cb();
+		async();
 	}.bind(this));
 };
 
@@ -96,89 +93,74 @@ UntetheredGenerator.prototype.app = function app() {
 };
 
 UntetheredGenerator.prototype.projectfiles = function () {
-	var async = this.async();
-	var cb = function() { console.log("UNTETHERED: projectfiles async."); async(); }.bind(this);
-
-	cp.exec('which git', function (error, stdout, stderr) {
-		if (/git/.test(stdout)) {
-			this.initGit(cb);
-		} else {
-			// no git, so quit now
-			cb();
-		}
-	}.bind(this));
-};
-
-UntetheredGenerator.prototype.initGit = function (cb) {
-	var self = this;
-	
-
-	if (!this.gitExists) {
-		console.log("No .git exists, ask if we should initialize");
-		var prompts = [{
+	var self = this,
+		async = this.async(),
+		prompts = [{
 			type: 'confirm',
-			name: 'gitinit',
+			name: 'gitInit',
 			message: 'Should I initialize a git repository for you?',
-			default: true
+			'default': true
+		}, {
+			type: 'confirm',
+			name: 'installGitHooks',
+			message: 'Do you want me to install the Git hooks?',
+			'default': true,
+			when: function (props) {
+				console.log("Prompt: installGitHooks when", props.gitInit, this.gitExists, arguments);
+
+				return props.gitInit || this.gitExists;
+			}.bind(this)
 		}];
 
-		this.prompt(prompts, function (props) {
-			console.log("Prompted for initialize, user said " + props.gitinit);
-			this.gitinit = props.gitinit;
-
-			if (props.gitinit) {
-				console.log("User said to initialize, exec git init");
-				cp.exec('git init', function (error, stdout, stderr) {
-					console.log("Git init complete");
-					if (!error) {
-						console.log("No error on git init, now call gitHooks");
-						self.gitHooks(cb);
-					} else {
-						console.log("Error on git init, time to quit");
-						// whoops, error, we're done here
-						console.log(error, stderr);
-						cb();
-					}
-				});
-			} else {
-				console.log("No .git folder and user doesn't want to initialize, time to quit");
-				// no .git folder and user doesn't want it initialized, we're done
-				cb();
-			}
-
-		}.bind(this));
-	} else {
-		console.log(".git alredy exists, ask if the user wants us to install the hooks");
-		this.gitHooks(cb);
-	}
-	console.log("initGit complete");
-};
-
-UntetheredGenerator.prototype.gitHooks = function (cb) {
-	console.log("GITHOOKS: ", cb);
-	var prompts = [];
-
-	prompts.push({
-		type: 'confirm',
-		name: 'installGitHooks',
-		message: 'Do you want me to install the Git hooks?',
-		default: true
-	});
-
 	this.prompt(prompts, function (props) {
-		console.log("GITHOOKS PROMPT: ", cb);
+		console.log(props);
+		this.gitInit = props.gitInit;
 		this.installGitHooks = props.installGitHooks;
 
-		if (props.installGitHooks) {
-			this.copy('.githooks/pre-commit', '.git/hooks/pre-commit');
+		if (props.gitInit) {
+			initGit.call(this, function () {
+				if (props.installGitHooks) {
+					installGitHooks.call(this);
+				}
+			}.bind(this));
 		}
 
-		// we're done whether or not the hooks are installed.
-		console.log("GITHOOKS PROMPT: ", cb);
-		cb();
-
+		async();
 	}.bind(this));
-	
-	console.log("gitHooks complete");
+};
 
-}
+/*
+----- Private Functions -----
+*/
+
+var initGit = function (cb) {
+	console.log("UntetheredGenerator: initGit");
+	
+	if(this.gitExists) 
+	{
+		console.log(".git already exists for this project, no need to initialize.");
+	}
+
+	if (this.gitInit && !this.gitExists) {
+		var async = this.async();
+
+		cp.exec('git init', function (error, stdout, stderr) {
+			if (error) {
+				console.log(error, stderr);
+			}
+
+			console.log("git init async");
+			async(error);
+		});
+	}
+	
+	if(cb)
+	{
+		cb();
+	}
+};
+
+var installGitHooks = function () {
+	console.log("UntetheredGenerator: installGitHooks");
+	this.copy('.githooks/pre-commit', '.git/hooks/pre-commit');
+};
